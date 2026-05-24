9452dadbb5bae0f6076020666ff18ce9773857c6
rassya <112611000+rsyafdhil@users.noreply.github.com>
2026-05-24 18:04:07 +0700

    Add button to call shopping van

--- Assets/POVCameraTES.cs
+++ Assets/POVCameraTES.cs
@@ -4,6 +4,10 @@ public class POVCameraTES : MonoBehaviour
 {
     public GameObject interactText;
 
+    [Header("Shop")]
+    public ShopNPC shopNPC; // ambil shopNPC yang udh ada di scene, nanti di inspector tinggal drag and drop aja
+    public GameObject povButtonCanvas; // canvas yang isinya tombol2
+
     private bool playerNear;
 
     void Update()
@@ -27,6 +31,7 @@ public class POVCameraTES : MonoBehaviour
         }
     }
 
+    // Kalau player keluar dari area, hide interact text dan close shop kalau kebuka
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -37,6 +42,34 @@ public class POVCameraTES : MonoBehaviour
                 interactText.SetActive(false);
 
             Debug.Log("Player exited cooking station");
+        }
+    }
+
+    // // Panggil button openShop di canvas
+    public void OpenShopFromPOV()
+    {
+        if (shopNPC != null)
+        {
+            shopNPC.OpenShop();
+        }
+    }
+
+    // Ketika tombol back dipencet nanti nutup toko dan switch camera ke main, jangan lupa unlock player movement
+    public void ClosePOV()
+    {
+        if (shopNPC != null)
+        {
+            shopNPC.CloseShop();
+            Debug.Log("Closed shop from POV");
+        }
+
+        CameraManager.Instance.SwitchToMain();
+        PlayerMovement.Instance?.LockMovement(false);
+        Debug.Log("Switched back to main camera from POV");
+
+        if (povButtonCanvas != null)
+        {
+            povButtonCanvas.SetActive(false);
         }
     }
 }
